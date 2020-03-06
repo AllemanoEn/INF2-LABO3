28bf4e3595640bda2d569d7abc8f761b5c1a364c
Enzo <enzo.allemano@hotmail.com>
2020-03-06 15:09:24 +0100

    Ajout des classes
    Joueur,carte,partie

---
+++ Carte.h
@@ -0,0 +1,14 @@
+//
+// Created by jetca on 06.03.2020.
+//
+
+#ifndef INF2_LABO3_CARTE_H
+#define INF2_LABO3_CARTE_H
+
+
+class Carte {
+
+};
+
+
+#endif //INF2_LABO3_CARTE_H
---
+++ Partie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jetca on 06.03.2020.
+//
+
+#include "Partie.h"
