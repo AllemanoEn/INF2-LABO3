70ed3f941e6ec854871e722f30fac9ce519efc14
Enzo <enzo.allemano@hotmail.com>
2020-03-10 10:39:50 +0100

    Implémentation de la classe Joueure

--- Joueur.h
+++ Joueur.h
@@ -1,13 +1,20 @@
-//
-// Created by jetca on 06.03.2020.
-//
-
 #ifndef INF2_LABO3_JOUEUR_H
 #define INF2_LABO3_JOUEUR_H
 
+#include <string>
+#include <vector>
+
+#include "Carte.h"
 
 class Joueur {
-
+    friend void echangerCarte();
+public:
+    void demanderCarte();
+    void detecterFamille();
+private:
+    std::string strNom;
+    std::vector<Carte> vCarteEnMain;
+    std::vector<Carte> vFamillesSurTable;
 };
 
 
