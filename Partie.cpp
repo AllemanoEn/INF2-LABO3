33da7df9c56497113be50d8422d0c9776bf8633d
Enzo <enzo.allemano@hotmail.com>
2020-03-10 10:57:36 +0100

    Classe Partie

--- Partie.cpp
+++ Partie.cpp
@@ -1,5 +1,1 @@
-//
-// Created by jetca on 06.03.2020.
-//
-
 #include "Partie.h"
--- Partie.h
+++ Partie.h
@@ -1,13 +1,16 @@
-//
-// Created by jetca on 06.03.2020.
-//
-
 #ifndef INF2_LABO3_PARTIE_H
 #define INF2_LABO3_PARTIE_H
 
+#include "Parametre.h"
+
 
 class Partie {
-
+public:
+    Partie(const unsigned nbrFamille,
+           const unsigned nbrCarteParFamille,
+           const unsigned nbrJoueurs,
+           const unsigned nbrCarteParJoueurs);
+private:
 };
 
 
