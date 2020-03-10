f497f80675571a05e50d5c7c796b294f76cef268
Enzo <enzo.allemano@hotmail.com>
2020-03-10 10:57:45 +0100

    Ajout des noms de joueurs

--- Parametre.h
+++ Parametre.h
@@ -1,13 +1,17 @@
 #ifndef INF2_LABO3_PARAMETRE_H
 #define INF2_LABO3_PARAMETRE_H
 
-<<<<<<< Updated upstream
+#include <string>
+
 const unsigned NOMBRE_FAMILLES = 9;
 const unsigned CARTES_PAR_FAMILLE = 4;
 const unsigned NOMBRE_JOUEURS = 4;
 const unsigned CARTES_PAR_JOUEUR = 5;
-=======
-const
->>>>>>> Stashed changes
+
+const std::string NOM_JOUEURS[] {"Alice",
+                                 "Bob",
+                                 "Corona",
+                                 "Dickael",
+                                 "Henso"};
 
 #endif //INF2_LABO3_PARAMETRE_H
