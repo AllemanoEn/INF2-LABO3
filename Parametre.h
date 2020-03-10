269e92b1075508d56f2bf5e854d69d11a0343fbb
Pellissier <dav.pellissier@gmail.com>
2020-03-10 10:57:25 +0100

    Constructeur Joueur

--- Parametre.h
+++ Parametre.h
@@ -1,13 +1,9 @@
 #ifndef INF2_LABO3_PARAMETRE_H
 #define INF2_LABO3_PARAMETRE_H
 
-<<<<<<< Updated upstream
 const unsigned NOMBRE_FAMILLES = 9;
 const unsigned CARTES_PAR_FAMILLE = 4;
 const unsigned NOMBRE_JOUEURS = 4;
 const unsigned CARTES_PAR_JOUEUR = 5;
-=======
-const
->>>>>>> Stashed changes
 
 #endif //INF2_LABO3_PARAMETRE_H
