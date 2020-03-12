cc4d28705f03bdc64f3d6b3eee5e09f1e8848eb4
Enzo <enzo.allemano@hotmail.com>
2020-03-12 14:46:09 +0100

    MAJ partie
    - Création du tas de pioche
    - Création des Joueurs avec le bon nombre de carte
    - Supprime les carte distribuées au joueurs du tas de carte

--- Partie.cpp
+++ Partie.cpp
@@ -1,7 +1,5 @@
 #include "Partie.h"
 
-#include <random>
-
 using namespace std;
 
 Partie::Partie(unsigned short nbrFamille, unsigned short nbrCarteParFamille, unsigned short nbrJoueurs, unsigned short nbrCarteParJoueurs) {
@@ -15,10 +13,14 @@ Partie::Partie(unsigned short nbrFamille, unsigned short nbrCarteParFamille, uns
     }
 
     //On mélange le jeu de carte
+    vector<Joueur> vJoueurs;
 
     //Creation des joueurs
-    for (int i = 0; i < nbrJoueurs; ++i) {
-
+    for (int i = 0,c = 0; i < nbrJoueurs; ++i,c+=CARTES_PAR_JOUEUR+1) {
+        vJoueurs.push_back(Joueur(NOM_JOUEURS[i],slice(vTasDePioche,c,c+CARTES_PAR_JOUEUR)));
     }
+
+    //On enlève les cartes du tas de pioche qui ont été distribuées
+    vTasDePioche.erase(vTasDePioche.begin(),vTasDePioche.begin()+(NOMBRE_JOUEURS*CARTES_PAR_JOUEUR));
 
 }
--- Partie.h
+++ Partie.h
@@ -14,6 +14,14 @@ public:
            unsigned short nbrCarteParFamille,
            unsigned short nbrJoueurs,
            unsigned short nbrCarteParJoueurs);
+    std::vector<Carte> slice(const std::vector<Carte>& v, int m, int n)
+    {
+        auto first = v.cbegin() + m;
+        auto last = v.cbegin() + n + 1;
+
+        std::vector<Carte> vec(first, last);
+        return vec;
+    }
 private:
     std::vector<Carte> vTasDePioche;
 };
