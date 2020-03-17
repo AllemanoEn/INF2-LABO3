9c9431b6ca36bba4b24ebc7630a64cf61bc9fce4
Enzo <enzo.allemano@hotmail.com>
2020-03-17 22:51:13 +0100

    main qui lance une partie

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
 
     // TODO: Comment faire pour lancer 100 parties sans avoir à les déclarer manuellement à chaque fois ?
-    Partie p1(NOMBRE_FAMILLES,CARTES_PAR_FAMILLE,NOMBRE_JOUEURS,CARTES_PAR_JOUEUR,NOM_JOUEURS);
+    Partie p1;
     p1.jouerPartie();
 
     cout << "Final test" << endl;
